11e29e7e8198f6ee4c25a34ea3097ae0e85b1d04
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2022-07-20 11:44:06 +0200

    Update

--- automate.sh
+++ automate.sh
@@ -1,12 +1,53 @@
+#!/bin/bash
 set -e
 
-#mv ~/Downloads/mcv-test/stego ~/Downloads/mcv-test/01-Improved-Spread-Spectrum
-#mkdir ~/Downloads/mcv-test/stego/
+BASE_DIR=/media/bjoern/Backup/dataset/
+TRAIN_DIR=$BASE_DIR/mcv-train
+TEST_DIR=$BASE_DIR/mcv-test
 
-#cd ~/development/audio-steganography-algorithms/02-Echo-Hiding/01-Echo-Hiding-Single-Kernel/
+for OUT_DIR in $TRAIN_DIR $TEST_DIR; do
+echo "[SET] $OUT_DIR"
 
-#find ~/Downloads/mcv-test/cover/female -name "*.mp3" | tr '\n' ' ' | xargs -n 250 ../../embed.sh text.txt asd /home/bjoern/Downloads/mcv-test/stego/female
-#mv ~/development/audio-steganography-algorithms/02-Echo-Hiding/01-Echo-Hiding-Single-Kernel/audio_out ~/Downloads/mcv-test/stego/female
+
+for ALGO in 01-Improved-Spread-Spectrum 02-Echo-Hiding-Single-Kernel; do
+DIR=~/development/audio-steganography-algorithms/$ALGO
+
+cd $DIR
+
+for CAT in female male other; do
+
+if [[ -d $OUT_DIR/$ALGO/$CAT ]]; then
+	echo "[SKIPPED] Algo: $ALGO Category: $CAT"
+else
+	echo "[UPDATE] Algo: $ALGO Category: $CAT"
+
+	rm -rf $DIR/audio_out
+	mkdir -p $DIR/audio_out
+	mkdir -p $OUT_DIR/$ALGO
+	find $OUT_DIR/cover/$CAT -name "*.wav" | ../../embed.sh text.txt asd $DIR/audio_out/
+	mv $DIR/audio_out $OUT_DIR/$ALGO/$CAT
+
+fi;
+
+done;
+done;
+done;
+
+
+cd ~/development/tf_audio_steganalysis/data_processing/python_scripts/
+
+for OUT_DIR in $TRAIN_DIR $TEST_DIR; do
+echo "[SET] $OUT_DIR"
+
+for ALGO in 01-Improved-Spread-Spectrum 02-Echo-Hiding-Single-Kernel 03-LSB-Coding 04-Phase-Coding; do
+
+for CAT in female male other; do
+	echo "[QMDCT] Algo: $ALGO Category: $CAT"
+
+	python3 QMDCT_extraction.py $OUT_DIR > /dev/null
+done;
+done;
+done;
 
 #find ~/Downloads/mcv-test/cover/male -name "*.mp3" | tr '\n' ' ' | xargs -n 250 ../../embed.sh text.txt asd /home/bjoern/Downloads/mcv-test/stego/male
 #mv ~/development/audio-steganography-algorithms/02-Echo-Hiding/01-Echo-Hiding-Single-Kernel/audio_out ~/Downloads/mcv-test/stego/male
@@ -16,9 +57,11 @@ set -e
 
 #mv ~/Downloads/mcv-test/stego/ ~/Downloads/mcv-test/02-Echo-Hiding-Single-Kernel
 
-cd ~/Downloads/mcv-test/
+#cd ~/Downloads/mcv-test/
 
-until rsync -avh . gpu:/project/datasets/MCV/testing
-do
-sleep .1
-done
+cd $TRAIN_DIR
+until rsync -avh . gpu:/project/datasets/MCV/training; do sleep .1; done;
+cd $TEST_DIR
+until rsync -avh . gpu:/project/datasets/MCV/testing; do sleep .1; done;
+done;
+
--- embed.sh
+++ embed.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
 if [ -z $1 ] || [ -z $2 ]; then
-	echo "Usage: $0 <textfile> <password> <outpath> <audio-in-path ...>"
+	echo "Usage: $0 <textfile> <password> <outpath>"
 	exit 1
 fi
 
 textfile=$1
 password=$2
 outpath=$3
-infiles=${@:4}
+# infiles=${@:4}
 
+prgm="tmp_embed"
 
-for f in $infiles; do
+rm -f "$prgm.m"
+echo "function file = $prgm()" > "$prgm.m"
+
+echo -n "input = [" >> "$prgm.m"
+while read f; do
 	file=$(basename "$f")
 	filepath=$(realpath $(dirname "$f"))
 
 	var="{'$file';'$filepath/'}"
-	vars=`[ -z "$vars" ] && echo "$var" || echo "$vars $var"`
+	echo -n "$var " >> "$prgm.m"
+	# vars=`[ -z "$vars" ] && echo "$var" || echo "$vars $var"`
 done;
-vars="[$vars]"
 
-program="input = $vars; for v = input; fprintf('embeding in file name: %s, file path: %s\n', v{1}, v{2}); data_embedding(v{1}, v{2}, '$textfile', '$password', '$outpath'); end; exit";
-echo "Executing MatLab Progam:
+echo "]" >> "$prgm.m"
+#vars="[$vars]"
 
-$program
+echo "for v = input;
+	fprintf('embeding in file name: %s, file path: %s\n', v{1}, v{2});
+	try
+		data_embedding(v{1}, v{2}, '$textfile', '$password', '$outpath');
+	catch ME
+		fprintf(2, 'Error while embedding in %s/%s: %s', v{2}, v{1}, getReport(ME))
+	end;
+	end;
+	exit" >> $prgm.m
 
-"
 
-matlab -nodisplay -r "$program"
+#program="function file = tmp_embed()
+#input = $vars; for v = input; fprintf('embeding in file name: %s, file path: %s\n', v{1}, v{2}); data_embedding(v{1}, v{2}, '$textfile', '$password', '$outpath'); end; exit";
+
+echo "Executing MatLab Progam..."
+
+#$program
+
+#"
+
+#echo "$program" > tmp_embed.m
+matlab -nodisplay -r "$prgm" > "$(date +'%Y-%m-%dT%H-%M-%S').log" && rm tmp_embed.m
 # Octave appears not to be able to run .p files
 # octave --eval "$program" -p $(pwd)
